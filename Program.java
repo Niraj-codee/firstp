f935e8016a602f06d66790a26b7cb4a81f5947c9
Niraj-codee <nirajkumar778087@gmail.com>
2024-12-13 22:32:18 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,16 @@
+
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Raj Prajapati");
+    }
+}
+
+
+
+
+// public class Program {
+//     public static void main(String[] args) {
+//         System.out.println("My Name is Raj Prajapati");
+//     }
+// }
